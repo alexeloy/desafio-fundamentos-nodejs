b2d52315aa8c5d8821564344c33232c7c1f25fdb
Alex Eloy <alexeloycg@gmail.com>
2020-06-25 23:30:46 -0300

    Desafio 05: Primeiro projeto Node.js

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,56 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    let { income, outcome, total } = this.balance;
+
+    if (type === 'income') {
+      income += value;
+      total += value;
+    } else {
+      outcome += value;
+      total -= value;
+    }
+
+    this.balance = {
+      ...this.balance,
+      income,
+      outcome,
+      total,
+    };
+
+    this.transactions.push(transaction);
+    return transaction; // TODO
   }
 }
 
